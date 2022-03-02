c27fbb836a0d2b42fed9cbd76d072c72ffa3e07f
YuviKaulge <100679348+YuviKaulge@users.noreply.github.com>
2022-03-02 22:44:41 +0530

    Update Demo.java

--- src/Demo.java
+++ src/Demo.java
@@ -12,6 +12,28 @@ public class Demo
 		 // Yuvikualge
 		System.out.println("Shrikant Kokate ");
 		System.out.println("Omerga");
+		   
+		int i=2;
+		int sum=19;
+		while(sum>i)
+		{
+			if(sum%i==0)
+			{
+				break;
+			}
+			i++;
+		}
+		if(i==sum)
+		{
+			System.out.println("Number Is Prime Number ");
+		}
+		else
+		{
+			System.out.println("Number Is not Primer Number");
+		}
+		
+
+		
 	}
 }
 
